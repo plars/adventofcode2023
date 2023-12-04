8b27a651a7d7ee692fa07fb1b8833263fdc795f1
Paul Larson <paul.larson@canonical.com>
2023-12-03 21:40:06 -0600

    day01b

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -22,7 +22,54 @@ fn get_calibration_value(line: &str) -> u32 {
     return first * 10 + last;
 }
 
+fn part_b(data: &str) {
+    let number_words = [
+        "one", "1", "two", "2", "three", "3", "four", "4", "five", "5", "six", "6", "seven", "7",
+        "eight", "8", "nine", "9",
+    ];
+    let mut sum = 0;
+    for line in data.split_whitespace() {
+        let mut first = 0;
+        let mut last = 0;
+
+        // find the first number
+        for i in 0..line.len() {
+            for (index, word) in number_words.iter().enumerate() {
+                if i + word.len() > line.len() {
+                    continue;
+                };
+                if line[i..i + word.len()].starts_with(word) {
+                    first = index / 2 + 1;
+                    break;
+                };
+            }
+            if first > 0 {
+                break;
+            }
+        }
+        // find the last number
+        for i in (0..line.len()).rev() {
+            for (index, word) in number_words.iter().enumerate() {
+                if i + word.len() > line.len() {
+                    continue;
+                };
+                if line[i..i + word.len()].starts_with(word) {
+                    last = index / 2 + 1;
+                    break;
+                };
+            }
+            if last > 0 {
+                break;
+            }
+        }
+        let calibration_value = first * 10 + last;
+        sum += calibration_value;
+    }
+    println!("sum: {}", sum)
+}
+
 fn main() {
     let data = include_str!("../input.txt");
     part_a(data);
+    part_b(data);
 }
