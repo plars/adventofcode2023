f7668c772859da45a1307460ab846deff349d584
Paul Larson <paul.larson@canonical.com>
2023-12-22 12:08:03 -0600

    day06a

---
+++ day06/src/main.rs
@@ -0,0 +1,29 @@
+fn main() {
+    let real_time_arr = [49, 78, 79, 80];
+    let real_distance_arr = [298, 1185, 1066, 1181];
+    let answer = part_a(&real_time_arr, &real_distance_arr);
+    println!("Part a answer: {answer}");
+}
+
+fn part_a(time_arr: &[u32], distance_arr: &[u32]) -> i32 {
+    let mut wins = Vec::new();
+    wins.resize(time_arr.len(), 0);
+
+    for race_num in 0..time_arr.len() {
+        for hold_time in 1..time_arr[race_num] {
+            if hold_time * (time_arr[race_num] - hold_time) > distance_arr[race_num] {
+                wins[race_num] += 1;
+            }
+        }
+    }
+    // return the product of elements in wins
+    wins.iter().product()
+}
+
+#[cfg(test)]
+#[test]
+fn test_part_a() {
+    let time_arr = [7, 15, 30];
+    let distance_arr = [9, 40, 200];
+    assert_eq!(part_a(&time_arr, &distance_arr), 288);
+}
